9fc5bcb2354e5c1827995dfa2e3da174c71e9072
Patrick Tangney <ptangney@outlook.com>
2022-07-14 13:07:54 +0100

    setup file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Article, Brand, CTA, Feature, Navbar } from './components';
+import {Footer, Features, Header, Possibility, WhatGP} from './containers';
 
 function App() {
   return (
     <div className="App">
-      hello world
-
+     <div className='gradient__bg'>
+      <Navbar />
+      <Header />
+     </div>
+     <Brand />
+     <WhatGP />
+     <Possibility />
+     <CTA />
+     <Blog />
+     <Footer />
     </div>
   );
 }
---
+++ src/containers/whatGPT3/WhatGP.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './whatGP.css'
+
+const WhatGP = () => {
+  return (
+    <div>WhatGP</div>
+  )
+}
+
+export default WhatGP
