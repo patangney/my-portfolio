6c148fb7ef77458a9280173176ee09dcdbe6c50a
Patrick Tangney <ptangney@outlook.com>
2022-07-14 13:47:28 +0100

    implement logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import {RiMenu3Line, RiCloseLine} from 'react-icons/ri'
+import CreativeIpsum from '../../assets/creativeIpsum.svg'
 import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className="gpt3__navbar-links_logo">
+          <img src={CreativeIpsum} alt="logo" />
+        </div>
+      </div>
+    </div>
   )
 }
 
